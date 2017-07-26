8db7ffbe342f3a276656fc10620e9a9a4fc128e3
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2017-07-25 23:58:03 -0400

    Updated Comments
    Added comments to further document my Kattis progress

--- Sup.java
+++ Sup.java
@@ -1,8 +1,16 @@
-package Hey;
+//
+// Bradley Lamitie
+// Solved 7/25/2017
+// Problem: Hello World!
+// Difficulty: 1.2/10
+// Current score: 1.2
+//
+
+package HelloWorld;
 
 import java.io.IOException;
 
-public class Sup {
+public class Hello {
 	public static void main(String[] args)throws IOException{
 		System.out.println("Hello World!");
 	}
